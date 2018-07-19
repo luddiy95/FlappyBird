9d012f671f8534736188b40f23ac12cd2ee29545
luddiy95 <6.romakun.6@gmail.com>
2018-07-19 09:58:36 +0900

    no message

---
+++ Assets/Scripts/Bird.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bird : MonoBehaviour {
+
+    public static bool deathzoneCol;
+
+	// Use this for initialization
+	void Start () {
+        deathzoneCol = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void Fly()
+    {
+        if(deathzoneCol == false)
+        {
+            this.GetComponent<Rigidbody2D>().velocity = new Vector2(0f, 1f);
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if(col.gameObject.tag == "deathzone")
+        {
+            deathzoneCol = true;
+            this.GetComponent<Rigidbody2D>().velocity = new Vector2(0f, 0f);
+            this.GetComponent<Rigidbody2D>().gravityScale = 0f;
+        }
+    }
+}
+
+
+
+
+
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour {
+
+    public GameObject Pype;
+    const int PypesNm = 10;
+    const float PypeInterval = 0.8f;
+    Queue<GameObject> Pypes = new Queue<GameObject>();
+
+    public GameObject GameOver;
+
+    int myRecord;
+    public GameObject TextMyRecord;
+    public GameObject TextBestRecord;
+
+	// Use this for initialization
+	void Start () {
+		for(int i = 0; i < PypesNm; i++)
+        {
+            int r = Random.Range(0, 9);
+            float y = -0.4f + (float)r / 10f;
+            float x = 2f + PypeInterval * (float)i;
+            Pypes.Enqueue(Instantiate(Pype, new Vector3(x, y, 0f), Quaternion.identity));
+        }
+        myRecord = 0;
+        if(PlayerPrefs.GetInt("BestRecord") > 0)
+        {
+
+        }
+        else
+        {
+            PlayerPrefs.SetInt("BestRecord", 0);
+        }
+        TextBestRecord.GetComponent<Text>().text = "BestRecord: " + PlayerPrefs.GetInt("BestRecord");
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if(Bird.deathzoneCol == false)
+        {
+            float headX = Pypes.Peek().transform.position.x;
+            if (-2f - 0.01f < headX && headX <= -2f)
+            {
+                Pypes.Dequeue();
+                float tailX = headX + PypeInterval * (PypesNm - 1);
+                int r = Random.Range(0, 9);
+                float ty = -0.4f + (float)r / 10f;
+                Pypes.Enqueue(Instantiate(Pype, new Vector3(tailX + PypeInterval, ty, 0f), Quaternion.identity));
+            }
+            foreach (GameObject pype in Pypes)
+            {
+                pype.transform.position += new Vector3(-0.01f, 0f, 0f);
+                float tx = pype.transform.position.x;
+                if(0f - 0.01f < tx && tx <= 0f)
+                {
+                    ++myRecord;
+                    TextMyRecord.GetComponent<Text>().text = "MyRecord: " + myRecord;
+                }
+            }
+        }
+        else
+        {
+            GameOver.SetActive(true);
+            if(myRecord > PlayerPrefs.GetInt("BestRecord"))
+            {
+                PlayerPrefs.SetInt("BestRecord", myRecord);
+                TextBestRecord.GetComponent<Text>().text = "BestRecord: " + myRecord;
+            }
+        }
+	}
+
+
+    public void ReTry()
+    {
+        Application.LoadLevel(Application.loadedLevel);
+    }
+}
